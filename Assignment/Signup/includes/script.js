7f7d9258c42d31fa250dcfc8e589edcfdd920709
Nishant <nishantkcr7@gmail.com>
2022-11-01 18:37:45 +0530

    completed form

--- Assignment/Signup/includes/script.js
+++ Assignment/Signup/includes/script.js
@@ -1,6 +1,9 @@
 // Signup Page Script
 // Getting reference of all input field and buttons
 
+let isSubmitClicked = false;
+hasError = false;
+
 const inputName = document.querySelector("#name");
 const inputDOB = document.querySelector("#dob");
 const inputSexValues = document.querySelectorAll("input[name='sex']");
@@ -17,59 +20,122 @@ const btnSubmit = document.querySelector("#submit");
 
 // Adding event listner on click of submit button
 btnSubmit.addEventListener("click", function () {
+  isSubmitClicked = !isSubmitClicked;
   validateForm();
+  activateBlur();
+  if (hasError) {
+    btnSubmit.setAttribute("disabled", true);
+  } else {
+    btnSubmit.setAttribute("disabled", false);
+  }
 });
 
 // Function to add error msg
 const dErrorMsg = (el, msg) => {
-  el.insertAdjacentHTML("afterend", `<span class="msg-error">${msg}</span>`);
+  el.nextElementSibling.innerText = msg;
   el.classList.add("error");
+};
+
+// Function to remove error msg
+const removeErrorMsg = (el) => {
+  el.nextElementSibling.innerText = "";
+  el.classList.remove("error");
 };
 
 // Function to validate form
 const validateForm = () => {
+  hasError = false;
   //   Validating Name
   if (inputName.value === "") {
     dErrorMsg(inputName, "Name can't be empty");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputName);
+    hasError = false;
   }
   //   Validating DOB
-  if (inputName.value === "") {
+  if (inputDOB.value === "") {
     dErrorMsg(inputDOB, "DOB can't be empty");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputDOB);
+    hasError = false;
   }
   //   Validating Sex
   let isGenderChecked = 0;
-  for (const sex of inputSexValues) {
-    if (sex.checked) {
-      isGenderChecked = 1;
-      break;
-    }
-  }
-  if (isGenderChecked === 0) {
+
+  if (document.querySelector("input[name='sex']:checked") == null) {
     dErrorMsg(inputSex, "Please choose your gender");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputSex);
+    hasError = false;
   }
+
   //   Validating Email
   if (inputEmail.value === "") {
     dErrorMsg(inputEmail, "Email can't be empty.");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputEmail);
+    hasError = false;
   }
 
   //   Validating Address
   if (inputAddress.value === "") {
     dErrorMsg(inputAddress, "Address can't be empty");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputAddress);
+    hasError = false;
   }
   //   Validating School
   if (inputSchool.value === "none") {
     dErrorMsg(inputSchool, "Please select school");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputSchool);
+    hasError = false;
   }
   //   Validating Department
   if (inputDepartment.value === "none") {
     dErrorMsg(inputDepartment, "Please select department");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputDepartment);
+    hasError = false;
   }
   //   Validating Course
   if (inputCourse.value === "none") {
     dErrorMsg(inputCourse, "Please select course");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputCourse);
+    hasError = false;
   }
   //   Validating Mobile
   if (inputMobile.value === "") {
     dErrorMsg(inputMobile, "Mobile can't be empty");
+    hasError = true;
+  } else {
+    removeErrorMsg(inputMobile);
+    hasError = false;
+  }
+  if (hasError == false) {
+    btnSubmit.setAttribute("disabled", false);
+  }
+  console.log(hasError);
+};
+
+const activateBlur = () => {
+  if (isSubmitClicked) {
+    inputName.addEventListener("blur", validateForm);
+    inputDOB.addEventListener("blur", validateForm);
+    inputEmail.addEventListener("blur", validateForm);
+    inputAddress.addEventListener("blur", validateForm);
+    inputSchool.addEventListener("blur", validateForm);
+    inputDepartment.addEventListener("blur", validateForm);
+    inputCourse.addEventListener("blur", validateForm);
+    inputMobile.addEventListener("blur", validateForm);
   }
 };
