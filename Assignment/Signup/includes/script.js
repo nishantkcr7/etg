c59f92ef8e80e7a1f3f6711fb87c88fa263881d3
Nishant <nishantkcr7@gmail.com>
2022-11-01 15:19:45 +0530

    signup form started

---
+++ Assignment/Signup/includes/script.js
@@ -0,0 +1,14 @@
+// Signup Page Script
+// Getting reference of all input field and buttons
+
+const inputName = document.querySelector("#name");
+const inputDOB = document.querySelector("#dob");
+const inputSex = document.querySelector("#sex");
+const inputEmail = document.querySelector("#email");
+const inputAddress = document.querySelector("#address");
+const inputSchool = document.querySelector("#school");
+const inputDepartment = document.querySelector("#department");
+const inputCourse = document.querySelector("#nameCourse");
+const inputMobile = document.querySelector("#mobile");
+const btnReset = document.querySelector("#reset");
+const btnSubmit = document.querySelector("#submit");
