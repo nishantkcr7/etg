0f34bf3eb703c0a4b633f1c1b92617dd151b9f40
Nishant <nishantkcr7@gmail.com>
2022-11-22 12:10:03 +0530

    slick slider added

---
+++ Assignment/Slider/assets/script.js
@@ -0,0 +1,13 @@
+$(".slider-info").slick({
+  dots: true,
+  infinite: true,
+  speed: 800,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+$(".slider-cricket").slick({
+  dots: true,
+  isFinite: true,
+  speed: 900,
+});
