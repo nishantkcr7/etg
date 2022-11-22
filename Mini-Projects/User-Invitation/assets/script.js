f004fd850941f67d77a7acd0cd72db782c36349e
Nishant <nishantkcr7@gmail.com>
2022-11-22 10:18:29 +0530

    slider assignment started

--- Mini-Projects/User-Invitation/assets/script.js
+++ Mini-Projects/User-Invitation/assets/script.js
@@ -1,3 +1,55 @@
 $(document).ready(() => {
-  alert("Loaded");
+  $("input[type='submit']").click(() => {
+    // Getting reference of input boxes
+    const inputName = $("input[name='name']");
+    const inputAge = $("input[name='age']");
+    // Getting Name from the user
+    const guestName = $(inputName).val();
+    // Getting Age from the user
+    const guestAge = $(inputAge).val();
+    // Validating the guest data
+
+    // Blank Input
+    guestName === ""
+      ? showError(inputName, "Please Enter Guest Name")
+      : hideError(inputName);
+
+    guestAge === "" && guestAge < 0
+      ? showError(inputAge, "Please Enter the Age")
+      : hideError(inputAge);
+    //   Age Less than 1 year
+    guestAge != "" && guestAge < 1
+      ? showError(inputAge, "Age can't be less than 1 year.")
+      : hideError(inputAge);
+  });
 });
+
+function showError(el, msg) {
+  el.next().text(msg);
+}
+
+function hideError(el) {
+  el.next().text("");
+}
+/*
+// Validating Age Using Promises
+function validateAge(age) {
+  return new Promise((resolve, reject) => {
+    if (age != "" && age > 0) {
+      resolve([true]);
+    }
+    if (age == "") {
+      reject([false, "Age can't be empty."]);
+    }
+    if (age != "" && age < 0) {
+      reject([false, "Age can't be less than 0."]);
+    }
+  })
+    .then((msg) => {
+      console.log(msg)
+    })
+    .catch((msg) => {
+      console.log(msg)
+    });
+}
+*/
